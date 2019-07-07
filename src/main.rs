c5ad5b2421212620e8f41b9ec77c0f632578972d
Thomas Kühnel <kuehnelth@gmail.com>
2019-07-07 22:44:13 +0200

    main returns result, use ipv4 only

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use mio::*;
 use mio::net::{TcpListener, TcpStream, UdpSocket};
-use std::net::SocketAddr;
+use std::net::{SocketAddr, ToSocketAddrs};
 use std::collections::HashMap;
 use multi_map::MultiMap;
+use std::*;
 
 struct TcpConnection {
     src: TcpStream,
@@ -14,25 +15,33 @@ struct UdpConnection {
     addr: SocketAddr,
 }
 
-fn main() {
+fn get_ipv4_socket_addr(input :&str) -> Result<SocketAddr, io::Error> {
+    let addrs_iter = input.to_socket_addrs()?;
+    for addr in addrs_iter {
+        if addr.is_ipv4() { return Ok(addr); }
+    }
+    Err(io::Error::new(io::ErrorKind::InvalidInput, "Can't resolve input to IPv4 socket address"))
+}
+
+fn main() -> Result<(), std::io::Error> {
     const TCP_SERVER: Token = Token(0);
     const UDP_SERVER: Token = Token(1);
     let mut next_token = 2;
     let mut tcp_conns = HashMap::with_capacity(32);
     let mut udp_conns = MultiMap::with_capacity(32);
 
-    let poll = Poll::new().unwrap();
+    let poll = Poll::new()?;
 
-    let addr = "0.0.0.0:1815".parse().unwrap();
-    let addr2 = "127.0.0.1:2815".parse().unwrap();
+    let addr = get_ipv4_socket_addr("0.0.0.0:1815")?;
+    let addr2 = get_ipv4_socket_addr("127.0.0.1:2815")?;
 
-    let tcp_server = TcpListener::bind(&addr).unwrap();
+    let tcp_server = TcpListener::bind(&addr)?;
     poll.register(&tcp_server, TCP_SERVER, Ready::readable(),
-                  PollOpt::edge()).unwrap();
+                  PollOpt::edge())?;
 
-    let udp_server = UdpSocket::bind(&addr).unwrap();
+    let udp_server = UdpSocket::bind(&addr)?;
     poll.register(&udp_server, UDP_SERVER, Ready::readable(),
-                  PollOpt::edge()).unwrap();
+                  PollOpt::edge())?;
 
 
     // Create storage for events
@@ -40,18 +49,18 @@ fn main() {
     let mut buf = [0; 8192];
 
     loop {
-        poll.poll(&mut events, None).unwrap();
+        poll.poll(&mut events, None)?;
 
         for event in events.iter() {
             match event.token() {
                 TCP_SERVER => {
                     let (stream1, _) = tcp_server.accept().unwrap();
                     poll.register(&stream1, Token(next_token), Ready::readable(),
-                                  PollOpt::edge()).unwrap();
+                                  PollOpt::edge())?;
                     next_token += 1;
                     let stream2 = TcpStream::connect(&addr2).unwrap();
                     poll.register(&stream2, Token(next_token), Ready::readable(),
-                                  PollOpt::edge()).unwrap();
+                                  PollOpt::edge())?;
                     next_token += 1;
 
                     let conn1 = TcpConnection{src: stream1, dst_id: next_token - 1};
@@ -66,7 +75,7 @@ fn main() {
                             let dst_sock = UdpSocket::bind(&addr).unwrap();
 
                             poll.register(&dst_sock, Token(next_token), Ready::readable(),
-                                          PollOpt::edge()).unwrap();
+                                          PollOpt::edge())?;
                             let conn = UdpConnection{src: dst_sock, addr: from};
                             udp_conns.insert(next_token, from, conn);
                             next_token += 1;
