0595418b2d6798506080bf28a235cb0c383128be
Thomas Kühnel <kuehnelth@gmail.com>
2019-10-03 14:58:23 +0200

    use wepoll instead of mio
    TODO: use epoll for linux build

--- src/portforwarder.rs
+++ src/portforwarder.rs
@@ -1,23 +1,27 @@
 use std::*;
-use mio::*;
-use mio::net::{TcpListener, TcpStream, UdpSocket};
+use std::io::{Read, Write};
+use wepoll_binding::{Epoll, EventFlag, Events};
+use std::net::{TcpListener, TcpStream, UdpSocket};
 use std::net::{IpAddr, Ipv4Addr, SocketAddr, ToSocketAddrs};
 use std::collections::HashMap;
 use multi_map::MultiMap;
-use log::{info, warn};
+use log::{debug, info, warn};
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::time::Duration;
+use std::{thread, time};
 
 struct TcpConnection {
     src: TcpStream,
-    dst_id: usize,
-    writable: bool,
+    dst_id: u64,
 }
 
 struct UdpConnection {
     src: UdpSocket,
     addr: SocketAddr,
 }
+
+#[derive(Copy, Clone, Eq, PartialEq, Debug)]
+pub struct Token(pub u64);
 
 pub fn get_ipv4_socket_addr(input :&String) -> Result<SocketAddr, io::Error> {
     let addrs_iter = input.to_socket_addrs()?;
@@ -28,21 +32,19 @@ pub fn get_ipv4_socket_addr(input :&String) -> Result<SocketAddr, io::Error> {
 }
 
 pub fn forward(src: SocketAddr, dst: SocketAddr, abort: Option<&AtomicBool>) -> Result<(), io::Error> {
-    const TCP_SERVER: Token = Token(0);
-    const UDP_SERVER: Token = Token(1);
+    const TCP_SERVER: u64 = 0;
+    const UDP_SERVER: u64 = 1;
     let mut next_token = 2;
     let mut tcp_conns = HashMap::with_capacity(32);
     let mut udp_conns = MultiMap::with_capacity(32);
 
-    let poll = Poll::new()?;
+    let epoll = Epoll::new()?;
 
-    let tcp_server = TcpListener::bind(&src)?;
-    poll.register(&tcp_server, TCP_SERVER, Ready::readable(),
-                  PollOpt::level())?;
+    let tcp_server = TcpListener::bind(src)?;
+    epoll.register(&tcp_server, EventFlag::IN, TCP_SERVER)?;
 
-    let udp_server = UdpSocket::bind(&src)?;
-    poll.register(&udp_server, UDP_SERVER, Ready::readable(),
-                  PollOpt::level())?;
+    let udp_server = UdpSocket::bind(src)?;
+    epoll.register(&udp_server, EventFlag::IN, UDP_SERVER)?;
 
 
     // Create storage for events
@@ -59,7 +61,7 @@ pub fn forward(src: SocketAddr, dst: SocketAddr, abort: Option<&AtomicBool>) -> 
     info!("Start forwarding from {} to {}", src, dst);
 
     loop {
-        poll.poll(&mut events, timeout)?;
+        epoll.poll(&mut events, timeout)?;
 
         if let Some(a) = abort {
             if a.load(Ordering::Relaxed) {
@@ -69,37 +71,42 @@ pub fn forward(src: SocketAddr, dst: SocketAddr, abort: Option<&AtomicBool>) -> 
         }
 
         for event in events.iter() {
-            //info!("event {:?}", event);
-            match event.token() {
+            debug!("event {:?}", event.data());
+            match event.data() {
                 TCP_SERVER => {
-                    /* connect happens async so we have to wait for a writable
-                     * event to know that the connection is established.
-                     * after that we will switch to listening for readable
-                     */
-                    let (stream1, from) = tcp_server.accept()?;
-                    info!("New TCP connection {:?}", from);
-                    poll.register(&stream1, Token(next_token), Ready::writable(),
-                                  PollOpt::level())?;
-                    next_token += 1;
-                    let stream2 = TcpStream::connect(&dst)?;
-                    poll.register(&stream2, Token(next_token), Ready::writable(),
-                                  PollOpt::level())?;
-                    next_token += 1;
+                        match tcp_server.accept() {
+                            Ok((stream1, from)) => {
+                                info!("New TCP connection {:?}", from);
+                                epoll.register(&stream1, EventFlag::IN, next_token)?;
+                                next_token += 1;
+                                let stream2 = TcpStream::connect(dst)?;
+                                epoll.register(&stream2, EventFlag::IN, next_token)?;
+                                next_token += 1;
 
-                    let conn1 = TcpConnection{src: stream1, dst_id: next_token - 1, writable: false};
-                    let conn2 = TcpConnection{src: stream2, dst_id: next_token - 2, writable: false};
-                    tcp_conns.insert(next_token - 2, conn1);
-                    tcp_conns.insert(next_token - 1, conn2);
+                                let conn1 = TcpConnection{src: stream1, dst_id: next_token - 1};
+                                let conn2 = TcpConnection{src: stream2, dst_id: next_token - 2};
+                                tcp_conns.insert(next_token - 2, conn1);
+                                tcp_conns.insert(next_token - 1, conn2);
+                                thread::sleep(time::Duration::from_millis(20));
+
+                            }
+                            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+                            }
+                            Err(e) => {
+                                warn!("error {:?}", e);
+                                return Err(e);
+                            }
+                        }
                 }
+
                 UDP_SERVER => {
                     if let Ok((len, from)) = udp_server.recv_from(&mut buf) {
                         if !udp_conns.contains_key_alt(&from) {
                             let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), 0);
-                            info!("read {} bytes udp from {:?}", len, from);
+                            debug!("read {} bytes udp from {:?}", len, from);
                             let dst_sock = UdpSocket::bind(&addr)?;
 
-                            poll.register(&dst_sock, Token(next_token), Ready::readable(),
-                                          PollOpt::level())?;
+                            epoll.register(&dst_sock, EventFlag::IN, next_token)?;
                             let conn = UdpConnection{src: dst_sock, addr: from};
                             udp_conns.insert(next_token, from, conn);
                             next_token += 1;
@@ -107,57 +114,46 @@ pub fn forward(src: SocketAddr, dst: SocketAddr, abort: Option<&AtomicBool>) -> 
 
                         if let Some(dst_conn) = udp_conns.get_alt(&from) {
                             let dst_sock = &dst_conn.src;
-                            info!("read {} bytes udp from {:?}", len, from);
+                            debug!("read {} bytes udp from {:?}", len, from);
                             dst_sock.send_to(&buf[..len], &dst)?;
                         }
                     }
                 }
-                Token(port) => {
-                    if event.readiness().is_writable() {
-                        if let Some(c) = tcp_conns.get_mut(&port) {
-                            poll.reregister(&c.src, Token(port), Ready::readable(),
-                                            PollOpt::level())?;
-                            c.writable = true;
-                            continue;
-                        }
-
-                    }
+                port => {
                     let mut to_remove = None;
-                    if let Some(c) = tcp_conns.get(&port) {
-                        if let Some(d) = tcp_conns.get(&c.dst_id) {
-                            if !d.writable {
-                                continue;
-                            }
-                        }
+                    let mut send_to = None;
+                    let mut send_len = 0;
+                    let mut buf = [0; 8192];
 
-                        let buffer_ref: &mut [u8] = &mut buf;
-                        let mut buffers: [&mut IoVec; 1] = [buffer_ref.into()];
-                        match c.src.read_bufs(&mut buffers) {
+                    if let Some(c) = tcp_conns.get_mut(&port) {
+                        match c.src.read(&mut buf) {
                             Ok(0) => {
                                 warn!("read {} bytes tcp from {:?}", 0, c.src.peer_addr()?);
                                 to_remove = Some((port, c.dst_id));
-                                /*
-                                if let Some(d) = tcp_conns.get(&c.dst_id) {
-                                    let d_buffers: [&IoVec; 0] = [];
-                                    d.src.write_bufs(&d_buffers)?;
-                                }
-                                */
                             }
                             Ok(len) => {
-                                info!("read {} bytes tcp from {:?}", len, c.src.peer_addr()?);
-                                if let Some(d) = tcp_conns.get(&c.dst_id) {
-                                    let d_buffers: [&IoVec; 1] = [buf[..len].into()];
-                                    d.src.write_bufs(&d_buffers)?;
-                                }
+                                debug!("read {} bytes tcp from {:?}", len, c.src.peer_addr()?);
+                                send_to = Some(c.dst_id);
+                                send_len = len;
                             }
                             Err(e) => {
                                 if e.kind() != io::ErrorKind::WouldBlock {
-                                    warn!("TCP error {:?}", e);
+                                    warn!("TCP error: {}", e);
                                     to_remove = Some((port, c.dst_id));
                                 }
                             }
                         }
                     }
+
+                    if let Some(dst_id) = send_to {
+                        if let Some(d) = tcp_conns.get_mut(&dst_id) {
+                            if let Err(e) = d.src.write(&buf[..send_len]) {
+                                warn!("TCP error {:?}", e);
+                                to_remove = Some((port, dst_id));
+                            }
+                        }
+                    }
+
                     if let Some((port1, port2)) = to_remove {
                         info!("Closing TCP connections {} and {}", port1, port2);
                         tcp_conns.remove(&port1);
@@ -167,19 +163,20 @@ pub fn forward(src: SocketAddr, dst: SocketAddr, abort: Option<&AtomicBool>) -> 
                     if let Some(c) = udp_conns.get(&port) {
                         match c.src.recv_from(&mut buf) {
                             Ok((len, from)) => {
-                                info!("read {} bytes udp from {:?}", len, from);
+                                debug!("read {} bytes udp from {:?}", len, from);
                                 let _ = udp_server.send_to(&buf[..len], &c.addr);
                             }
                             Err(e) => {
-                                //if e.kind() != io::ErrorKind::WouldBlock {
-                                    warn!("UDP error {:?}", e);
-                                //}
+                                warn!("UDP error {:?}", e);
                             }
                         }
                     }
                 },
+
             }
         }
+
+
     }
 
     Ok(())
